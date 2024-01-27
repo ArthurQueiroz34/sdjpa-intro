e1aa1d5c959c017ba22361410fc66770f289028a
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-01-27 18:47:13 -0300

    Adding Table using Flyway

--- src/main/resources/db.migration/V1.sql
+++ src/main/resources/db.migration/V1.sql
@@ -1,10 +1,16 @@
-DROP DATABASE IF EXISTS bookdb;
-DROP USER IF EXISTS `bookadmin`@`%`;
-DROP USER IF EXISTS `bookuser`@`%`;
-CREATE DATABASE IF NOT EXISTS bookbd CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `bookadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `bookdb`.* TO `bookadmin`@`%`;
-CREATE USER IF NOT EXISTS `bookuser`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `bookdb`.* TO `bookuser`@`%`;
-FLUSH PRIVILEGES;
+drop table if exists book;
+drop table if exists book_seq;
+
+create table book (
+                      id bigint not null,
+                      isbn varchar(255),
+                      publisher varchar(255),
+                      title varchar(255),
+                      primary key (id)
+) engine=InnoDB;
+
+create table book_seq (
+    next_val bigint
+) engine=InnoDB;
+
+insert into book_seq values ( 1 );
