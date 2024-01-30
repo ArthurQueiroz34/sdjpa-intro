00a8915b71a8645ba7fe97088fe4755dc5f899dc
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-01-29 22:27:56 -0300

    UUID RFC 4122 Primary Key

--- src/main/java/com/excalibur/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/excalibur/sdjpaintro/bootstrap/DataInitializer.java
@@ -2,19 +2,23 @@ package com.excalibur.sdjpaintro.bootstrap;
 
 import com.excalibur.sdjpaintro.domain.AuthorUuid;
 import com.excalibur.sdjpaintro.domain.Book;
+import com.excalibur.sdjpaintro.domain.BookUuid;
 import com.excalibur.sdjpaintro.repositories.AuthorUuidRepository;
 import com.excalibur.sdjpaintro.repositories.BookRepository;
 
+import com.excalibur.sdjpaintro.repositories.BookUuidRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorUuidRepository authorUuidRepository;
+    private final BookUuidRepository bookUuidRepository;
 
-    public DataInitializer(BookRepository bookRepository, AuthorUuidRepository authorUuidRepository) {
+    public DataInitializer(BookRepository bookRepository, AuthorUuidRepository authorUuidRepository, BookUuidRepository bookUuidRepository) {
         this.bookRepository = bookRepository;
         this.authorUuidRepository = authorUuidRepository;
+        this.bookUuidRepository = bookUuidRepository;
     }
 
     @Override
@@ -36,6 +40,11 @@ public class DataInitializer implements CommandLineRunner {
         authorUuid.setFirstName("Alexia");
         authorUuid.setLastName("Straza");
         AuthorUuid savedAuthor = authorUuidRepository.save(authorUuid);
+        System.out.println("Saved Author UUID: " + savedAuthor.getId());
+
+        BookUuid bookUuid = new BookUuid();
+        bookUuid.setTitle("All About UUIDs");
+        BookUuid savedBookUuid = bookUuidRepository.save(bookUuid);
         System.out.println("Saved Author UUID: " + savedAuthor.getId());
     }
 }
---
+++ src/main/java/com/excalibur/sdjpaintro/repositories/BookUuidRepository.java
@@ -0,0 +1,9 @@
+package com.excalibur.sdjpaintro.repositories;
+
+import com.excalibur.sdjpaintro.domain.BookUuid;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookUuidRepository extends JpaRepository<BookUuid, UUID> {
+}
