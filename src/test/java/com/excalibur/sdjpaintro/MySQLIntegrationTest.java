e1915ec347fb576b2f0ceb44684a1b6c52f00c03
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-01-29 23:24:31 -0300

    Unit tests for UUID Primary Keys

--- src/test/java/com/excalibur/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/com/excalibur/sdjpaintro/MySQLIntegrationTest.java
@@ -1,7 +1,11 @@
 package com.excalibur.sdjpaintro;
 
+import com.excalibur.sdjpaintro.domain.AuthorUuid;
+import com.excalibur.sdjpaintro.domain.BookUuid;
+import com.excalibur.sdjpaintro.repositories.AuthorUuidRepository;
 import com.excalibur.sdjpaintro.repositories.BookRepository;
 
+import com.excalibur.sdjpaintro.repositories.BookUuidRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,6 +23,33 @@ public class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
+    @Test
+    void testBookUuid() {
+        BookUuid bookUuid = bookUuidRepository.save(new BookUuid());
+        assertThat(bookUuid).isNotNull();
+        assertThat(bookUuid.getId());
+
+        BookUuid fetched = bookUuidRepository.getById(bookUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
+    @Test
+    void testAuthorUuid() {
+        AuthorUuid authorUuid = authorUuidRepository.save(new AuthorUuid());
+        assertThat(authorUuid).isNotNull();
+        assertThat(authorUuid.getId()).isNotNull();
+
+        AuthorUuid fetched = authorUuidRepository.getById(authorUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
 
     @Test
     void testMySQL() {
